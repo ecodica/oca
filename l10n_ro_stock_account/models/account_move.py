50438cd05807395c9035cde5bdd0beb1f8b3194a
Goran Kliska <goran@ecodica.eu>
2026-03-30 14:45:03 +0200

    OCA Update

--- l10n_ro_stock_account/models/account_move.py
+++ l10n_ro_stock_account/models/account_move.py
@@ -16,6 +16,14 @@ class AccountMove(models.Model):
         readonly=True,
     )
 
+    def _stock_account_prepare_anglo_saxon_in_lines_vals(self):
+        l10n_ro_moves = self.filtered(lambda m: m.company_id.l10n_ro_accounting)
+        if l10n_ro_moves == self:
+            return []
+        return super(
+            AccountMove, self - l10n_ro_moves
+        )._stock_account_prepare_anglo_saxon_in_lines_vals()
+
     def _stock_account_prepare_realtime_out_lines_vals(self):
         # nu se mai face descarcarea de gestiune la facturare
         ro_invoices = self.filtered(lambda inv: inv.is_l10n_ro_record)
@@ -31,7 +39,7 @@ class AccountMove(models.Model):
             if move.is_l10n_ro_record:
                 stock_moves = move.line_ids._get_stock_moves()
                 if stock_moves and all(
-                    "return" in m.l10n_ro_move_type
+                    "return" in (m.l10n_ro_move_type or "")
                     or m.l10n_ro_move_type == "plus_inventory"
                     for m in stock_moves
                 ):
